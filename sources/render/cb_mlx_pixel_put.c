324a3ba8a3ab2668e2289e32bc655df2fb83531b
salyce <salyce@student.21-school.ru>
2021-12-24 21:49:52 +0300

    12

--- sources/render/cb_mlx_pixel_put.c
+++
@@ -1,9 +0,0 @@
-#include "../../includes/cub3d.h"
-
-void	cb_mlx_pixel_put(t_win *win, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = win->addr + (y * win->line_length + x * (win->bpp / 8));
-	*(unsigned int *)dst = color;
-}
--- sources/utils/ft_degree_to_ratio.c
+++
@@ -1,6 +0,0 @@
-#include "../../includes/cub3d.h"
-
-float	ft_degree_to_ratio(float degree)
-{
-	return (degree * acos(-1.0) / 180);
-}
